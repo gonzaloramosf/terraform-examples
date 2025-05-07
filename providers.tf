4c05f3ff0f33074c41498117b1bdb321597ab7aa
Gonzalo Ramos Farinho <gonzalo@Gonzalos-MacBook-Pro.local>
2025-05-06 23:19:56 -0300

    s3 tfstate

--- providers.tf
+++ providers.tf
@@ -1,4 +1,12 @@
 terraform {
+  required_version = ">= 1.10.5"
+
+  # backend "s3" {
+  #   bucket = "liftoff-tf-s3-bucket"
+  #   key = "liftoff/state.tfstate"
+  #   region = "us-east-1"
+  # }
+
   required_providers {
     aws = {
       source = "hashicorp/aws"
